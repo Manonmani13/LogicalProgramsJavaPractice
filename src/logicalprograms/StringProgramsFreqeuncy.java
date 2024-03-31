c6c5fc0ed195238ee036fdb608e46073103c2637
Manonmani13 <manonmani.pandukuma@gmail.com>
2024-03-31 21:41:12 +0530

    # count of characters

---
+++ src/logicalprograms/StringProgramsFreqeuncy.java
@@ -0,0 +1,34 @@
+package logicalprograms;
+
+public class StringProgramsFreqeuncy {
+public static void main(String[] args) {
+	String name="manonmani";
+	
+	int count=1;
+	String op="";
+	for(int i=0;i<name.length();i++)
+	{
+		if(!op.contains(String.valueOf(name.charAt(i)))) {
+		op+=name.charAt(i);
+	
+			for(int j=i+1;j<name.length();j++)
+			{
+				if(name.charAt(i)==name.charAt(j) )
+				{
+					++count;
+
+				}
+			}
+			
+			System.out.println(" op "+name.charAt(i)+ count);
+count=1;
+//System.out.println(op.contains("m"));
+		}
+	}
+	
+		
+		
+		
+	
+}
+}
