3044fc8943596b81cb10d6156803dd3777b78360
Manonmani13 <manonmani.pandukuma@gmail.com>
2024-04-01 20:56:16 +0530

    #reverse string

---
+++ src/logicalprograms/ReverseString.java
@@ -0,0 +1,11 @@
+package logicalprograms;
+
+public class ReverseString {
+public static void main(String[] args) {
+	String name="manonmani";
+    for(int i=name.length()-1;i>=0;i--)
+    {
+        System.out.print(name.charAt(i));
+    }
+}
+}
