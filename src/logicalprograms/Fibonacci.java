005e2abb36547364995ca47497c54b0dba39c56e
Manonmani13 <manonmani.pandukuma@gmail.com>
2024-04-02 22:20:20 +0530

    Fibonacci

---
+++ src/logicalprograms/Fibonacci.java
@@ -0,0 +1,18 @@
+package logicalprograms;
+
+public class Fibonacci {
+public static void main(String[] args) {
+	int n=0,n1=1,n3=0,num=10;
+	for(int i=1;i<=num;i++)
+	{
+		System.out.println(n3);
+
+		n=n1;
+		n1=n3;
+		n3=n+n1;
+
+
+	}
+	
+}
+}
