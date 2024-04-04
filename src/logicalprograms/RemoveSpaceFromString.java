e587906951b3ed973b242e3dcb57e1f0ee923c28
Manonmani13 <manonmani.pandukuma@gmail.com>
2024-04-04 08:33:21 +0530

    remove space

---
+++ src/logicalprograms/RemoveSpaceFromString.java
@@ -0,0 +1,9 @@
+package logicalprograms;
+
+public class RemoveSpaceFromString {
+public static void main(String[] args) {
+	String name="manonmani loves everything";
+	String repl=name.replace(" ", "");
+	System.out.println(repl);
+}
+}
