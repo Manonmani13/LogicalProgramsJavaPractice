d089dd69b4bd26abad9b1282eb409650c144970f
Manonmani13 <manonmani.pandukuma@gmail.com>
2024-04-04 08:34:04 +0530

    sort

---
+++ src/logicalprograms/ArraySort.java
@@ -0,0 +1,11 @@
+package logicalprograms;
+
+import java.util.Arrays;
+
+public class ArraySort {
+public static void main(String[] args) {
+	int n[]= {2,3,4,-1,0,-2,-3,6,5,7};
+	Arrays.sort(n);
+	System.out.println(Arrays.toString(n));
+}
+}
