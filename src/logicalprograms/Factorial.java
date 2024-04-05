1191c9a9d86e282042140470f61d23af6dfba627
ManonmaniPandukumar <86237230+Manonmani13@users.noreply.github.com>
2024-04-05 23:41:41 +0530

    Add files via upload

---
+++ src/logicalprograms/Factorial.java
@@ -0,0 +1,14 @@
+package logicalprograms;
+
+public class Factorial {
+public static void main(String[] args) {
+	int n=5,s=1;
+	for(int i=1;i<=5;i++)
+	{
+		
+		s=s*i;
+	}
+	System.out.println(s);
+
+}
+}
