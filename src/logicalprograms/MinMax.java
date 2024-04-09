b678bfdebe99c1d21b4fe8bcd35354edfd08f2ee
ManonmaniPandukumar <86237230+Manonmani13@users.noreply.github.com>
2024-04-09 22:28:02 +0530

    Add files via upload

---
+++ src/logicalprograms/MinMax.java
@@ -0,0 +1,18 @@
+package logicalprogram.arrays;
+
+public class MinMax {
+public static void main(String[] args) {
+	int a[]= {5,10,20,30,100,50,60,0,80,1};
+	int min=a[0],max=a[0];
+	for(int i=0;i<a.length;i++) {
+		if(max<a[i]) {
+			max=a[i];
+		}
+		if(min>a[i])
+		{
+			min=a[i];
+		}
+	}
+	System.out.println(max+" "+min);
+}
+}
