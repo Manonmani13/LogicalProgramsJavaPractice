88dc1a86fa629b79fbc6cb680a41a18e8d8095d7
ManonmaniPandukumar <86237230+Manonmani13@users.noreply.github.com>
2024-04-09 23:00:02 +0530

    Add files via upload

---
+++ src/logicalprograms/ReverseArray.java
@@ -0,0 +1,11 @@
+package logicalprogram.arrays;
+
+public class ReverseArray {
+	public static void main(String[] args) {
+		int a[]= {5,10,20,30,100,50,60,0,80,1};
+		for(int i=a.length-1;i>=0;i--) {
+			System.out.println(a[i]);
+
+		}
+	}
+}
