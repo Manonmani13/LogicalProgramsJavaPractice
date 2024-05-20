d43d6ee14349f87901dec2df905572b5ae8992a6
ManonmaniPandukumar <86237230+Manonmani13@users.noreply.github.com>
2024-05-20 22:12:16 +0530

    Add files via upload

---
+++ src/MininumNumberOfPlatform.java
@@ -0,0 +1,23 @@
+package com.proretention.scrm.admin;
+
+public class MininumNumberOfPlatform {
+    public static void main(String[] args) {
+        int nums[]={20,10,20,30,15,30,70,10,4};
+        int count=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            for(int j=0;j<nums.length;j++)
+            {
+                if(nums[i]==nums[j])
+                {
+                    count++;
+                }
+            }
+            if(count%2==1)
+                System.out.println(nums[i]);
+            count=0;
+
+        }
+
+    }
+}
