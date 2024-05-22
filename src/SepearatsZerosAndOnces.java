d005653e310ff4a7ca198139589e75661bace50d
ManonmaniPandukumar <86237230+Manonmani13@users.noreply.github.com>
2024-05-22 22:20:15 +0530

    Add files via upload

---
+++ src/SepearatsZerosAndOnces.java
@@ -0,0 +1,21 @@
+package com.proretention.scrm.admin;
+
+import java.util.Arrays;
+
+public class SepearatsZerosAndOnces {
+    public static void main(String[] args) {
+        int a[]={0,1,0,0,1,0,1,0,1,1,1,0};
+        //Method 1
+        Arrays.sort(a);
+
+        for(int i=0;i<=a.length-1;i++)
+        {
+            System.out.println(a[i]);
+        }
+        for(int i=0;i<=a.length-1;i++)
+        {
+            System.out.println(a[i]);
+        }
+
+    }
+}
