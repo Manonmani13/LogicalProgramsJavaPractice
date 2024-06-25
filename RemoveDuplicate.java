30ca05718651318a3e11cf2938b4a523e87bacd5
ManonmaniPandukumar <86237230+Manonmani13@users.noreply.github.com>
2024-06-25 22:27:14 +0530

    Add files via upload

---
+++ RemoveDuplicate.java
@@ -0,0 +1,20 @@
+package com.proretention.scrm.calendar;
+
+public class RemoveDuplicate {
+    public static void main(String[] args) {
+        int arr[]={2,3,4,4,5,5,6,6};
+
+        for(int i=0;i<arr.length;i++)
+        {
+            for(int j=i+1;j< arr.length;j++)
+            {
+                if(arr[i]==arr[j]){
+                    System.out.println(arr[i]);
+                    break;
+                }
+
+            }
+        }
+
+    }
+}
